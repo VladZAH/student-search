5ea96ac31725bf71648d28a5a5dbb33aeb68b794
Volodymyr Zahorodnyy <vovazahorodniy@gmail.com>
2020-11-04 11:14:41 -0800

    Updates

---
+++ precache-manifest.bdc49db51559f21dafb4a0e886d090ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ddfe8234f9ae481355b6175b233c131",
+    "url": "/student-search/index.html"
+  },
+  {
+    "revision": "6a2b72ceed5ba5f254e9",
+    "url": "/student-search/static/css/main.ceef917f.chunk.css"
+  },
+  {
+    "revision": "0823fb84d4e2aa790682",
+    "url": "/student-search/static/js/2.05d50806.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/student-search/static/js/2.05d50806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2b72ceed5ba5f254e9",
+    "url": "/student-search/static/js/main.e4899f28.chunk.js"
+  },
+  {
+    "revision": "daff55b49c35691a655e",
+    "url": "/student-search/static/js/runtime-main.06138705.js"
+  }
+]);
